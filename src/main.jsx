8e4d79d8e31cf44b9872a464eea0f4db33e293ca
Nahuelsand <rod.arena7@gmail.com>
2026-04-14 02:23:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Si no tienes este archivo, puedes crearlo vacío o borrar esta línea
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
